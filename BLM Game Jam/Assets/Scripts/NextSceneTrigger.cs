0a41ea43357b797c227b70036ca9c43315a0ae9e
Edilio Vera <ediliovera97@gmail.com>
2020-07-17 12:58:04 -0500

    added next scene trigger for goal and test next scene that loops back

---
+++ BLM Game Jam/Assets/Scripts/NextSceneTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NextSceneTrigger : MonoBehaviour
+{
+    [System.Serializable]
+    public class StringEvent : UnityEvent<string> { }
+
+    [SerializeField]
+    public StringEvent nextSceneMethod;
+    public string nextSceneName;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        nextSceneMethod.Invoke(nextSceneName);
+    }
+}

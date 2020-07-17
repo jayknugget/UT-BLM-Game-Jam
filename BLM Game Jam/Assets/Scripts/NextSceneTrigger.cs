a53b697765f249e657871935009934dd7c07cce8
digocorbellini <digocorbellini@gmail.com>
2020-07-17 16:45:58 -0500

    Two colored doors
    both must be touched by same colored player to go to the next level.

--- BLM Game Jam/Assets/Scripts/NextSceneTrigger.cs
+++ BLM Game Jam/Assets/Scripts/NextSceneTrigger.cs
@@ -5,15 +5,21 @@ using UnityEngine.Events;
 
 public class NextSceneTrigger : MonoBehaviour
 {
+    
     [System.Serializable]
     public class StringEvent : UnityEvent<string> { }
 
     [SerializeField]
     public StringEvent nextSceneMethod;
     public string nextSceneName;
+    public GameObject allowedPlayer;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        nextSceneMethod.Invoke(nextSceneName);
+        if(allowedPlayer.Equals(collision.gameObject)){
+            nextSceneMethod.Invoke(nextSceneName);
+            collision.gameObject.SetActive(false);
+        }
+        
     }
 }
--- BLM Game Jam/Assets/Scripts/SceneController.cs
+++ BLM Game Jam/Assets/Scripts/SceneController.cs
@@ -12,6 +12,20 @@ public class SceneController : MonoBehaviour
     /**
      * Find the next scene name within the Build Settings
      */
+
+    private static bool greenHasEntered;
+    private static bool redHasEntered;
+    void Awake()
+    {
+        greenHasEntered = false;
+        redHasEntered = false;
+    }
+    void Update()
+    {
+        if(greenHasEntered&&redHasEntered){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
     public void GoToScene(string nextScene)
     {
         SceneManager.LoadScene(nextScene);
@@ -23,5 +37,13 @@ public class SceneController : MonoBehaviour
         AsyncOperation sceneLoading = 
             SceneManager.LoadSceneAsync(nextScene);
         // loading bar?
+    }
+
+    public void greenCompleted(){
+        greenHasEntered = true;
+    }
+
+    public void redCompleted(){
+        redHasEntered = true;
     }
 }

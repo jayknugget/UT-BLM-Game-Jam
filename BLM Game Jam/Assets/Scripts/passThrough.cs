aabec62b005f844c2930b823ad3d647baf27b8e4
digocorbellini <digocorbellini@gmail.com>
2020-07-17 00:25:19 -0500

    Head Jump
    Can jump on the heads of players now
    
    also added space for idle anim in playerController script.

--- BLM Game Jam/Assets/Scripts/passThrough.cs
+++ BLM Game Jam/Assets/Scripts/passThrough.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class passThrough : MonoBehaviour
 {
     public GameObject playerToIgnore;
+    public GameObject topOfPlayer;
 
     // Update is called once per frame
     void Update()
     {
         Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(),playerToIgnore.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(),topOfPlayer.GetComponent<Collider2D>());
     }
 }

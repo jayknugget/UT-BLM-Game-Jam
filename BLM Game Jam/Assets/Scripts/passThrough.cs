75a12f4bd1359d3d95b4c513120d0e2bb753bd0c
digocorbellini <digocorbellini@gmail.com>
2020-07-16 23:40:48 -0500

    Colored walls
    Players can go through walls of the same color.

---
+++ BLM Game Jam/Assets/Scripts/passThrough.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class passThrough : MonoBehaviour
+{
+    public GameObject playerToIgnore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(),playerToIgnore.GetComponent<Collider2D>());
+    }
+}

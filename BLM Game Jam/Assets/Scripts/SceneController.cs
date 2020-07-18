d3890421d42e8ce10f0b04b3f51a89672157205c
digocorbellini <digocorbellini@gmail.com>
2020-07-18 17:18:19 -0500

    Less slippy boys

---
+++ BLM Game Jam/Assets/Scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Place inside new EmptyObject SceneController to access functions from
+ * other objects via event or pointer.
+ */
+public class SceneController : MonoBehaviour
+{
+    /**
+     * Find the next scene name within the Build Settings
+     */
+    public void GoToScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void GoToLoadingScene(string nextScene)
+    {
+        GoToScene("LoadingScene");
+        AsyncOperation sceneLoading = 
+            SceneManager.LoadSceneAsync(nextScene);
+        // loading bar?
+    }
+
+}

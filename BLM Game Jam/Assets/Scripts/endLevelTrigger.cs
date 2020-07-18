7cb63857bb7c1d9e56f9ebd7e69509dbef9a7e5e
Edilio Vera <ediliovera97@gmail.com>
2020-07-18 17:21:28 -0500

    Levels1-3 done, Level 4 in progress

--- BLM Game Jam/Assets/Scripts/endLevelTrigger.cs
+++ BLM Game Jam/Assets/Scripts/endLevelTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //Place this on different colored doors.
 public class endLevelTrigger : MonoBehaviour
@@ -9,7 +10,19 @@ public class endLevelTrigger : MonoBehaviour
     private static bool greenHasFinished;
     private static bool redHasFinished;
     public string allowedPlayerTag;
-    public string nextSceneName;
+    public volatile string nextSceneName;
+    public bool isLevel;
+
+    void Start()
+    {
+        if (isLevel)
+        {
+            string sceneName = SceneManager.GetActiveScene().name;
+            int sceneNum = int.Parse(sceneName.Substring("Level".Length));
+            nextSceneName = "Level" + (sceneNum + 1);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == allowedPlayerTag){

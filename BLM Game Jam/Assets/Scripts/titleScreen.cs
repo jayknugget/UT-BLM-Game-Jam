30f66a2af0b846b7f58eee614a149e5f3dc9faf6
digocorbellini <digocorbellini@gmail.com>
2020-07-18 19:29:18 -0500

    Made a basic Title Screen
    No background yet. Maybe we could get art for the controls?

---
+++ BLM Game Jam/Assets/Scripts/titleScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleScreen : MonoBehaviour
+{
+    public string firstLevelName;
+    public GameObject title;
+    public GameObject controls;
+
+    public void startGame(){
+        SceneControl.GoToScene(firstLevelName);
+    }
+
+    public void quitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void controlsScreen(){
+        title.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    public void backToTitleScreen(){
+        title.SetActive(true);
+        controls.SetActive(false);
+    }
+}

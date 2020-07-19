21c76c5c1d91a17efddd3f5d56fcaf56a01dda8c
Edilio Vera <ediliovera97@gmail.com>
2020-07-19 10:53:21 -0500

    dimscreen, dynamic camera update, and flow controller

---
+++ BLM Game Jam/Assets/Scripts/DimScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DimScreen : MonoBehaviour
+{
+    public static float lerpT = 0.05f;
+
+    public int dimStart;
+    public int dimEnd;
+    public bool isDimming;
+
+    private Image dimScreen;
+    private bool oldDim;
+
+    void Awake()
+    {
+        dimScreen = GetComponent<Image>();
+    }
+
+    public void DimIn()
+    {
+        Dim(dimEnd);
+    }
+
+    public void DimOut()
+    {
+        Dim(dimStart);
+    }
+
+    private void Dim(int iTarget)
+    {
+        if (!isDimming)
+        {
+            isDimming = true;
+            StartCoroutine(DimChangeCoroutine(iTarget));
+        }
+    }
+
+    IEnumerator DimChangeCoroutine(int iTarget)
+    {
+        Color dimColor = dimScreen.color;
+        float lerpAlpha = dimScreen.color.a;
+        float fTarget = (float)iTarget / 255;
+
+        while (Mathf.Abs(lerpAlpha - fTarget) > 0.001)
+        {
+            lerpAlpha = Mathf.Lerp(lerpAlpha, fTarget, lerpT);
+            Color newColor = new Color(dimColor.r, dimColor.g, dimColor.b, lerpAlpha);
+            dimScreen.color = newColor;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        isDimming = false;
+    }
+}
---
+++ BLM Game Jam/Assets/Scripts/LevelFlowController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFlowController : MonoBehaviour
+{
+    public playerControler playerController;    // for enabling and disabling player movement
+    public GameObject tutorialCanvas;           // for displaying tutorial notecards
+    public DynamicCamera levelCamera;           // for zooming in and out and handing over control
+    public pauseMenu pauseMenu;                 // for knowing when game is pawsed
+
+    private DimScreen dimScreen;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (tutorialCanvas != null)
+        {
+            dimScreen = tutorialCanvas.GetComponentInChildren<DimScreen>();
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(Tutorial1());
+    }
+
+    IEnumerator Tutorial1()
+    {
+        playerController.enabled = false;   // disable player input
+        dimScreen.DimIn();
+        while (dimScreen.isDimming)         // wait for screen to dim in
+        {
+            yield return null;
+        }
+        // TODO: show tutorial notecard
+        yield return new WaitForSeconds(3); // debounce player input so they don't skip it
+
+        while (!Input.anyKeyDown)           // wait for the user to press a key
+        {
+            yield return null;
+        }
+        // TODO: show text to press any button
+
+        dimScreen.DimOut();
+        while (dimScreen.isDimming)         // wait for screen to dim in
+        {
+            yield return null;
+        }
+
+        playerController.enabled = true;    // enable player input
+        levelCamera.normalMode = true;      // zoom in to the players
+    }
+}
